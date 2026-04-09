41aadf139c5ed04b42afa009b14cbb4af125f078
Richard Pausch <r.pausch@hzdr.de>
2026-04-09 21:32:16 +0200

    dd automation to create vocab db

--- tools/create_vocab_jap_db.py
+++ tools/create_vocab_jap_db.py
@@ -3,6 +3,17 @@ sys.path.append('../lib/')
 
 from vocabTrainer import VocabTrainer
 
+list_vocab_pairs = []
+
+list_vocab_pairs.append(("すし", "Sushi"))
+list_vocab_pairs.append(("みず", "Wasser"))
+
+
 if __name__ == "__main__":
     vocab_trainer = VocabTrainer()
-    
+
+    for word, translation in list_vocab_pairs:
+        vocab_trainer.add_word(word, translation)
+
+    print("Vocab list:")
+    vocab_trainer.print_all_vocab()
