858bb6368f49d1fb25221e1600c26a4d0e2f7933
Richard Pausch <r.pausch@hzdr.de>
2026-04-11 23:01:18 +0200

    add more vocab

--- tools/create_vocab_jap_db.py
+++ tools/create_vocab_jap_db.py
@@ -7,6 +7,17 @@ list_vocab_pairs = []
 
 list_vocab_pairs.append(("すし", "Sushi"))
 list_vocab_pairs.append(("みず", "Wasser"))
+list_vocab_pairs.append(("ください", "bitte"))
+list_vocab_pairs.append(("ごはん", "Reis"))
+list_vocab_pairs.append(("です", "da ist / es ist / ich bin"))
+list_vocab_pairs.append(("いしゃ", "Arzt"))
+list_vocab_pairs.append(("せんせい", "Lehrer"))
+list_vocab_pairs.append(("やさしい", "einfach / nett"))
+list_vocab_pairs.append(("べんごし", "Anwalt"))
+list_vocab_pairs.append(("かっこいい", "cool"))
+list_vocab_pairs.append(("ひと", "Person"))
+list_vocab_pairs.append(("がくせい", "Schüler"))
+list_vocab_pairs.append(("けん", "Ken / Ticket"))
 
 
 if __name__ == "__main__":
