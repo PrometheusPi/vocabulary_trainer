6209f87a22c326a21dca5d97ee959fcd42731a8d
Richard Pausch <r.pausch@hzdr.de>
2026-04-12 23:27:04 +0200

    add more vocab

--- tools/create_vocab_jap_db.py
+++ tools/create_vocab_jap_db.py
@@ -18,6 +18,18 @@ list_vocab_pairs.append(("かっこいい", "cool"))
 list_vocab_pairs.append(("ひと", "Person"))
 list_vocab_pairs.append(("がくせい", "Schüler"))
 list_vocab_pairs.append(("けん", "Ken / Ticket"))
+list_vocab_pairs.append(("こんにちは", "hallo / hi"))
+list_vocab_pairs.append(("どうぞよろしく", "schön dich kennenzulernen"))
+list_vocab_pairs.append(("はな", "Blume"))
+list_vocab_pairs.append(("なおみ", "Naomi"))
+list_vocab_pairs.append(("さん", "Herr / Frau"))
+list_vocab_pairs.append(("こんばんは", "guten Abend / heute Abend"))
+list_vocab_pairs.append(("はい", "ja"))
+list_vocab_pairs.append(("いいえ", "nein"))
+list_vocab_pairs.append(("にほんじん", "Japaner"))
+list_vocab_pairs.append(("カナダ", "Kanada"))
+list_vocab_pairs.append(("アメリカじん", "Amerikaner"))
+
 
 
 if __name__ == "__main__":
