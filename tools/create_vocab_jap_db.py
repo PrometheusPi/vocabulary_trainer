a23e8ca7342d56d4b2c6f49251eb07b462f4d0f9
Richard Pausch <r.pausch@hzdr.de>
2026-04-13 21:50:28 +0200

    restructure vocab list

--- tools/create_vocab_jap_db.py
+++ tools/create_vocab_jap_db.py
@@ -3,33 +3,32 @@ sys.path.append('../lib/')
 
 from vocabTrainer import VocabTrainer
 
-list_vocab_pairs = []
-
-list_vocab_pairs.append(("すし", "Sushi"))
-list_vocab_pairs.append(("みず", "Wasser"))
-list_vocab_pairs.append(("ください", "bitte"))
-list_vocab_pairs.append(("ごはん", "Reis"))
-list_vocab_pairs.append(("です", "da ist / es ist / ich bin"))
-list_vocab_pairs.append(("いしゃ", "Arzt"))
-list_vocab_pairs.append(("せんせい", "Lehrer"))
-list_vocab_pairs.append(("やさしい", "einfach / nett"))
-list_vocab_pairs.append(("べんごし", "Anwalt"))
-list_vocab_pairs.append(("かっこいい", "cool"))
-list_vocab_pairs.append(("ひと", "Person"))
-list_vocab_pairs.append(("がくせい", "Schüler"))
-list_vocab_pairs.append(("けん", "Ken / Ticket"))
-list_vocab_pairs.append(("こんにちは", "hallo / hi"))
-list_vocab_pairs.append(("どうぞよろしく", "schön dich kennenzulernen"))
-list_vocab_pairs.append(("はな", "Blume"))
-list_vocab_pairs.append(("なおみ", "Naomi"))
-list_vocab_pairs.append(("さん", "Herr / Frau"))
-list_vocab_pairs.append(("こんばんは", "guten Abend / heute Abend"))
-list_vocab_pairs.append(("はい", "ja"))
-list_vocab_pairs.append(("いいえ", "nein"))
-list_vocab_pairs.append(("にほんじん", "Japaner"))
-list_vocab_pairs.append(("カナダ", "Kanada"))
-list_vocab_pairs.append(("アメリカじん", "Amerikaner"))
-
+list_vocab_pairs = [
+    ("すし", "Sushi"),
+    ("みず", "Wasser"),
+    ("ください", "bitte"),
+    ("ごはん", "Reis"),
+    ("です", "da ist / es ist / ich bin"),
+    ("いしゃ", "Arzt"),
+    ("せんせい", "Lehrer"),
+    ("やさしい", "einfach / nett"),
+    ("べんごし", "Anwalt"),
+    ("かっこいい", "cool"),
+    ("ひと", "Person"),
+    ("がくせい", "Schüler"),
+    ("けん", "Ken / Ticket"),
+    ("こんにちは", "hallo / hi"),
+    ("どうぞよろしく", "schön dich kennenzulernen"),
+    ("はな", "Blume"),
+    ("なおみ", "Naomi"),
+    ("さん", "Herr / Frau"),
+    ("こんばんは", "guten Abend / heute Abend"),
+    ("はい", "ja"),
+    ("いいえ", "nein"),
+    ("にほんじん", "Japaner"),
+    ("カナダ", "Kanada"),
+    ("アメリカじん", "Amerikaner"),
+    ]
 
 
 if __name__ == "__main__":
