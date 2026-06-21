598aebb04e59fe47f3c81291357443d53b4db9c5
Richard Pausch <r.pausch@hzdr.de>
2026-06-21 22:20:36 +0200

    add info on number of vacab entries

--- tools/create_vocab_jap_db.py
+++ tools/create_vocab_jap_db.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     vocab_trainer.create_vocab_db("Japanisch_Deutsch")
     for word, translation in list_vocab_pairs_jap_deu:
         vocab_trainer.add_word(word, translation, "Japanisch_Deutsch")
+    print("Number of entries:", len(list_vocab_pairs_jap_deu))
 
     vocab_trainer.create_vocab_db("Italienisch_Deutsch")
     for word, translation in list_vocab_pairs_ital_deu:
         vocab_trainer.add_word(word, translation, "Italienisch_Deutsch")
-
+    print("Number of entries:", len(list_vocab_pairs_ital_deu))
 
     print("Languages:")
     for lang in vocab_trainer.get_all_languages():
